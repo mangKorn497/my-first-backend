61833d7200e15eac42f9ca3925428d63ceea6924
Chayanon <mangkorn1234.aa@gmail.com>
2026-06-25 16:41:15 +0700

    update server.js

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ import { createServer } from "http";
 const server = createServer((req, res) => {
   const { method, url } = req;
   res.setHeader("Content-Type", "application/json");
-  if (method === "GET" && url === "/") {
+  if (method === "GET" && url === "/xyz") {
     res.writeHead(200);
     res.end(
       JSON.stringify({
-        message: "สวัสดีจาก Node.js Server!",
+        message: "สวัสดีจาก POST Method XYZ",
       }),
     );
   } else {
     res.writeHead(404);
-    res.end(JSON.stringify({ error: "ไมพบหนาที่ตองการ" }));
+    res.end(JSON.stringify({ error: "ไม่พบหน้าที่ต้องการ" }));
   }
 });
 server.listen(3000, () => {
-  console.log("Server รันอยูที่ http://localhost:3000");
+  console.log("Server รันอยู่ที่ http://localhost:3000");
 });
